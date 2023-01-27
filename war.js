8c72cf501dc4102852a5699fade659f74f2aa74a
Kelsey Brown <119462221+KelseyBrown90s@users.noreply.github.com>
2023-01-26 19:57:03 -0600

    Add files via upload

---
+++ war.js
@@ -0,0 +1,40 @@
+class Card {
+constructor(suitType,cardValue){
+    this.suitType = suitType;
+    this.cardValue = cardValue;
+    let suits = [clubs,spades,hearts,diamonds]; 
+    let cardNames = [two,three,four,five,six,seven,eight,nine,ten,jack,queen,king,ace];
+    let two = 2;
+    let three= 3;
+    let four = 4;
+    let five = 5;
+    let six = 6;
+    let seven = 7;
+    let eight = 8;
+    let nine = 9;
+    let ten = 10;
+    let jack = 11;
+    let queen = 12;
+    let king = 13;
+    let ace =14;
+
+    let cardNumber
+}
+
+
+}
+class Deck {
+    constructor(wholeDeck){
+         this.wholeDeck= wholeDeck;
+    }
+}
+class Player {
+    constructor(name,score,amountOfCards,player1,player2){
+        this.name = name;
+        this.score = score;
+        this.amountofCards = amountOfCards;
+        this.player1 = player1;
+        this.player2 = player2;
+
+}
+}
